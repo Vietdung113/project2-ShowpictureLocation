46d655dcbd2b1c5d8f11692378c570e94e561ec2
Doan Viet Dung <doanvietdung273@gmail.com>
2017-03-29 02:33:34 +0700

    create new project

---
+++ showpicturelocation_project2/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace showpicturelocation_project2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.LongText = new System.Windows.Forms.TextBox();
+            this.latText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(646, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(45, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(156, 245);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(207, 27);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(303, 250);
+            this.webBrowser1.TabIndex = 2;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // LongText
+            // 
+            this.LongText.Location = new System.Drawing.Point(576, 36);
+            this.LongText.Name = "LongText";
+            this.LongText.Size = new System.Drawing.Size(58, 20);
+            this.LongText.TabIndex = 3;
+            this.LongText.TextChanged += new System.EventHandler(this.LongText_TextChanged);
+            // 
+            // latText
+            // 
+            this.latText.Location = new System.Drawing.Point(576, 62);
+            this.latText.Name = "latText";
+            this.latText.Size = new System.Drawing.Size(58, 20);
+            this.latText.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(559, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(513, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Longtitude";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(525, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Latitude";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 288);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.latText);
+            this.Controls.Add(this.LongText);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox LongText;
+        private System.Windows.Forms.TextBox latText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ showpicturelocation_project2/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace showpicturelocation_project2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ofd.Title = "Input Picture";
+            ofd.Filter = "JPG|*.jgp|JPEG|*.jpeg|PNG|*.png";
+            DialogResult dr = ofd.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(ofd.FileName);
+            }
+            string url = location(ofd.FileName);
+            webBrowser1.Navigate(url);
+            
+        }
+
+
+        public string location(string path_to_file)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(path_to_file))
+                {
+                    MessageBox.Show("No file passed in");
+                    return null;
+                }
+                GeoPoint gp = null;
+                try
+                {
+                    gp = GeoPoint.GetFromImageFile(path_to_file);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error getting coordinates from image file. ");
+
+                    return null;
+                }
+                if (gp != null && gp.IsValid)
+                {
+                    string url = AppSetting("MAP_URL_MASK", @"https://maps.google.com/maps?q=##LAT_COORD##,##LONG_COORD##");
+                    string coord_mask = AppSetting("COORD_FORMAT_MASK", Coord.DEFAULT_COORD_MASK);
+                    bool encode_coord = true;
+                    try
+                    {
+                        encode_coord = Boolean.Parse(
+                            AppSetting("ENCODE_COORD", encode_coord.ToString())
+                            );
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error getting url encodong details.  Ensure app config is present and has proper settings.");
+                    }
+                    Func<string, string> CoordEncoder = new Func<string, string>(s => s);
+                   /*
+                    if (encode_coord)
+                    {
+                        CoordEncoder = new Func<string, string>(s => System.Web.HttpUtility.UrlEncode(s));
+                    }
+                    */
+                    latText.Text = CoordEncoder(gp.Latitude.ToGeoCode().ToString());
+                    LongText.Text = CoordEncoder(gp.Longitude.ToGeoCode().ToString());                
+                    url = url
+                        .Replace("##LAT_COORD##", CoordEncoder(gp.Latitude.ToString(coord_mask)))
+                        .Replace("##LONG_COORD##", CoordEncoder(gp.Longitude.ToString(coord_mask)))
+                        .Replace("##LAT_GEO##", latText.Text)
+                        .Replace("##LONG_GEO##", LongText.Text);
+                    //string url = AppSetting
+                    return url;
+                }
+                else
+                {
+                    MessageBox.Show("Unable to resolve coordinates");
+                    return null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unknown failure, see additional details.");
+                return null;
+            }
+            
+        }
+
+
+        private static string AppSetting(string setting, string error_default)
+        {
+            try
+            {
+                string value = System.Configuration.ConfigurationSettings.AppSettings[setting];
+                if (!String.IsNullOrEmpty(value))
+                {
+                    return value;
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+
+            return error_default;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LongText_TextChanged(object sender, EventArgs e)
+        {
+            string url = AppSetting("MAP_URL_MASK", @"https://maps.google.com/maps?q=##LAT_COORD##,##LONG_COORD##");
+            string coord_mask = AppSetting("COORD_FORMAT_MASK", Coord.DEFAULT_COORD_MASK);
+            bool encode_coord = true;
+            Func<string, string> CoordEncoder = new Func<string, string>(s => s);
+            GeoPoint gp = null;
+            gp = GeoPoint.GetFromImageFile(ofd.FileName);
+            url = url
+                        .Replace("##LAT_COORD##", CoordEncoder(gp.Latitude.ToString(coord_mask)))
+                        .Replace("##LONG_COORD##", CoordEncoder(gp.Longitude.ToString(coord_mask)))
+                        .Replace("##LAT_GEO##", latText.Text)
+                        .Replace("##LONG_GEO##", LongText.Text);
+            webBrowser1.Navigate(url);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (e.Url == webBrowser1.Url)
+            {
+                if (webBrowser1.ReadyState == WebBrowserReadyState.Complete)
+                {
+                    TakeScreenshot(webBrowser1);
+                }
+            }
+        }
+        protected void TakeScreenshot(WebBrowser wb)
+        {
+            Size pageSize = new Size(wb.Document.Window.Size.Width, wb.Document.Window.Size.Height);
+        }
+    }
+}
